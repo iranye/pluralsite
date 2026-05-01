4810e883347e7f04a8b0843c4a0894a61ba93fd2
IraNye <inye@hchb.com>
2026-05-01 16:52:26 -0400

    impl PieController

---
+++ Gill/ASPNET_Core_Fundamentals/BethanysPieShop/BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,24 @@
+﻿using BethanysPieShop.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository pieRepository;
+        private readonly ICategoryRepository categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            this.pieRepository = pieRepository ?? throw new ArgumentNullException(nameof(pieRepository));
+            this.categoryRepository = categoryRepository ?? throw new ArgumentNullException(nameof(categoryRepository));
+        }
+
+        public IActionResult List()
+        {
+            ViewBag.CurrentCategory = "Cheese cakes";
+
+            return View(pieRepository.AllPies);
+        }
+    }
+}
