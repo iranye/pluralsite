43cc7d10cb31bb3dfef274b08092ba35a936e8a6
Ira Nye <iranye@gmail.com>
2026-05-03 08:50:39 -0400

    SW aspnet Best-practice add supporing files

---
+++ ShawnW/ASP.NET_10_BestPrax_EntValidnModels/QUERIES.sql
@@ -0,0 +1,17 @@
+USE JurisDb -- ON (localdb)\MSSQLLocalDB
+
+SELECT *
+from dbo.Cases
+
+SELECT *
+from dbo.Clients
+
+SELECT *
+from dbo.Employees
+
+SELECT *
+FROM dbo.Invoices
+	
+
+SELECT *
+FROM dbo.TimeBills
